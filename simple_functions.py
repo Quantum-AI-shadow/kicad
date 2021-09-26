0eb63a95d644be0f52bf9f583347e5a4531bb67d
Quantum-AI-shadow <i.quantum.ai@gmail.com>
2021-09-26 10:55:14 +0530

    Second edit

--- simple_functions.py
+++ simple_functions.py
@@ -10,7 +10,7 @@ def get_track_list(filename):
     board = pcbnew.LoadBoard(filename)
     tracks = board.GetTracks()
     for track in tracks:
-        if(type(track) is not pcbnew.VIA):
+        if(type(track) is pcbnew.TRACK):
             track_list.append(track)
             track_pos_list.append([ToMM(track.GetStart()), ToMM(track.GetEnd())])
             track_layer_list.append(track.GetLayerName())
@@ -18,8 +18,26 @@ def get_track_list(filename):
             track_width_list.append(track.GetWidth()/1e6) # Converting to mm
     return [track_pos_list, track_layer_list, track_net_list, track_width_list]   # change the return to whatever you want
 
+def get_via_list(filename):
+    via_list       = []
+    via_drill_list = []
+    via_width_list = []
+    via_net_list   = []
+    board = pcbnew.LoadBoard(filename)
+    tracks = board.GetTracks()
+    for via in tracks:
+        if(type(via) is pcbnew.VIA):
+            via_list.append(via)
+            via_width_list.append(via.GetWidth()/1e6)
+            via_drill_list.append(via.GetDrill()/1e6)
+            via_net_list.append(via.GetNetname())
+    return [via_width_list, via_drill_list, via_net_list]
 
 ans = get_track_list("/home/quantum/Desktop/kicad/arduno_mini/arduno_mini.kicad_pcb")
+for i in ans:
+    print(i)
+    print()
+ans = get_via_list("/home/quantum/Desktop/kicad/arduno_mini/arduno_mini.kicad_pcb")
 for i in ans:
     print(i)
     print()
