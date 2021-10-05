73148ab6622c356d79f6f40f723079b4c5136bbc
Quantum-AI-shadow <i.quantum.ai@gmail.com>
2021-10-05 23:18:03 +0530

    commit

--- create.py
+++ create.py
@@ -3,8 +3,9 @@ from pcbnew import *
 
 '''
 exec(open('/home/quantum/Desktop/git_repo/kicad/create.py').read())
-
 '''
+
+board = pcbnew.GetBoard()
 
 def get_component(name):
     return [x for x in board.GetModules() if x.GetReference() == name][0]
@@ -14,6 +15,12 @@ def getnetID(netName):
     netCode = nets.find(netName).value()[1].GetNet()
     return netCode
 
+def getLayerID(layerName):
+    for i in range(pcbnew.PCB_LAYER_ID_COUNT):
+        if(board.GetLayerName(i) == layerName):
+            layerID = i
+            return layerID
+
 def addVia(x, y, drill, width, netName, refresh = 1):
     newVia = pcbnew.VIA(board)
     newVia.SetPosition(pcbnew.wxPoint(x*1e6, y*1e6))
@@ -21,5 +28,15 @@ def addVia(x, y, drill, width, netName, refresh = 1):
     newVia.SetWidth(int(width*1e6))
     newVia.SetNetCode(getnetID(netName))
     board.Add(newVia)
+    if(refresh == 1):
+        pcbnew.Refresh()
+
+def addTrack(x1, y1, x2, y2, trackWidth, netName, layerName, refresh = 1):
+    track = pcbnew.TRACK(board)
+    track.SetStart(pcbnew.wxPoint(int(x1*1e6), int(y1*1e6)))
+    track.SetEnd(pcbnew.wxPoint(int(x2*1e6), int(y2*1e6)))
+    track.SetNetCode(getnetID(netName))
+    track.SetLayer(getLayerID(layerName))
+    board.Add(track)
     if(refresh == 1):
         pcbnew.Refresh()
